200b28d9366940feba5bcd103291e4ff8bc7ba81
CR531 <chakradhar.lsg4@gmail.com>
2019-10-20 20:31:19 -0500

    Edited Server.js

--- src/api/server.js
+++ src/api/server.js
@@ -10,9 +10,9 @@ const printOrderRoute = require('./printOrder.route');
 
 mongoose.Promise = global.Promise;
 mongoose.set('useCreateIndex', true);
-mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+mongoose.connect(config.DB, { useNewUrlParser: true, useUnifiedTopology: true }).then(
     () => { console.log('Database is connected') },
-    err => { console.log('Can not connect to the database' + err) }
+    err => { console.log('Cannot connect to the database' + err) }
 );
 
 app.use(cors());
